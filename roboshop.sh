79cc09269ebde4817ebb5b983dfa13333a3b0f93
vinay davin <vinaydavin@gmail.com>
2026-01-12 23:23:37 +0530

    1

--- roboshop.sh
+++ roboshop.sh
@@ -5,10 +5,11 @@ sg_id="sg-05ff4bd44c0bc3ba5"
 subnet_id="subnet-04d4f1ebfd040d45e"
 zone_id="Z08339141ZNV0KWJ5D2UQ"
 domain_name="vdavin.online"
+key="vdavin key.pem"
 
 for instance in $@
 do
-    inst_id=$(aws ec2 run-instances --image-id $ami_id --instance-type t3.micro --subnet-id $subnet_id --security-group-ids $sg_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    inst_id=$(aws ec2 run-instances --image-id $ami_id --instance-type t3.micro --subnet-id $subnet_id --security-group-ids $sg_id --key-name $key --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 if [ $instance != "frontend" ]; then
     IP=$(aws ec2 describe-instances --instance-ids $inst_id --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
     rec_name="$instance.$domain_name"
