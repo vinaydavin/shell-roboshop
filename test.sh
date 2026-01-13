0a884b52d66c7ef151ec3380ad20cf7703c26256
vinay davin <vinaydavin@gmail.com>
2026-01-13 14:43:15 +0530

    1

--- test.sh
+++ test.sh
@@ -1,8 +1,7 @@
 #! /bin/bash
 a=$1
-
-  echo "aws ec2 describe-instances \
+aws ec2 describe-instances \
 --filters "Name=private-ip-address,Values=$1" \
 --query "Reservations[].Instances[].InstanceId" \
---output text"
-"
+--output text
+
