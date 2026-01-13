8a7bd08abe9182fb943a0130969c6e2ab5d0f885
vinay davin <vinaydavin@gmail.com>
2026-01-13 15:05:46 +0530

    1

--- roboshop.sh
+++ roboshop.sh
@@ -43,4 +43,4 @@ done
 pub_ip=$(aws ec2 describe-instances --instance-ids $inst_id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
     rec_name="$domain_name"
 
-echo "ssh -i /"vdavin-pem.pem/" ec2-user@${pub_ip}.compute-1.amazonaws.com"
+echo "ssh -i \"vdavin-pem.pem\" ec2-user@${pub_ip}.compute-1.amazonaws.com"
