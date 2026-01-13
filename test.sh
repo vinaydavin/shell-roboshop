20ef8b6b9108ec2420d45b161eac8c6aff1cfebd
vinay davin <vinaydavin@gmail.com>
2026-01-13 14:41:21 +0530

    1

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #! /bin/bash
 a=$1
 
-  echo "{aws ec2 describe-instances --filters "Name=private-ip-address,Values=$a" --query "Reservations[].Instances
-[].{InstanceId:InstanceId,PrivateIP:PrivateIpAddress,PublicIP:PublicIpAddress}" --output table}"
+  echo "aws ec2 describe-instances \
+--filters "Name=private-ip-address,Values=$1" \
+--query "Reservations[].Instances[].InstanceId" \
+--output text
+"
