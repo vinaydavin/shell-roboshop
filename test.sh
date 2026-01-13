f192d3e15d219ba4e960c8eb889a93d16d16b15c
vinay davin <vinaydavin@gmail.com>
2026-01-13 14:33:18 +0530

    1

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-read $1
+a=$1
 
-  aws ec2 describe-instances --filters "Name=private-ip-address,Values=$1" --query "Reservations[].Instances
+  aws ec2 describe-instances --filters "Name=private-ip-address,Values=$a" --query "Reservations[].Instances
 [].{InstanceId:InstanceId,PrivateIP:PrivateIpAddress,PublicIP:PublicIpAddress}" --output table
