32a8242e909fc3c4acccdaf548329055654e6233
vinay davin <vinaydavin@gmail.com>
2026-01-13 15:15:52 +0530

    1

--- roboshop.sh
+++ roboshop.sh
@@ -42,6 +42,6 @@ done
 
 pub_ip=$(aws ec2 describe-instances --instance-ids $inst_id --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
     rec_name="$domain_name"
-PUB-IP=$(echo "$pub_ip" | sed 's/\./-/g')
+PUB_IP=$(echo "$pub_ip" | sed 's/\./-/g')
 
-echo "ssh -i \"vdavin-pem.pem\" ec2-user@ec2-${PUB-IP}.compute-1.amazonaws.com"
+echo "ssh -i \"vdavin-pem.pem\" ec2-user@ec2-${PUB_IP}.compute-1.amazonaws.com"
